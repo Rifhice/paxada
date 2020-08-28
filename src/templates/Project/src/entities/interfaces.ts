0b8dc0e81c626543b1e2413c7bd12b42d7dcbdf3
Kevin Giordani <giordani.kevin.02@gmail.com>
2020-08-28 15:30:36 +0200

    valid entity generation

---
+++ src/templates/Project/src/entities/interfaces.ts
@@ -0,0 +1,9 @@
+export type CreatedListener<T> = (created: T | T[]) => any;
+export type UpdatedListener<T> = (updated: T | T[], old?: T | T[]) => any;
+export type DeletedListener<T> = (deleted: T | T[]) => any;
+
+export interface DAOEvents<T> {
+    created: CreatedListener<T>;
+    deleted: UpdatedListener<T>;
+    updated: DeletedListener<T>;
+}
